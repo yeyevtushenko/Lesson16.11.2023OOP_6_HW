9aadc6ea680ec8ab6037ad697659d3e6535323de
yeyevtushenko <yehor803@gmail.com>
2023-12-25 20:50:28 +0200

    Для класів із першого завдання перевизначте магічні методи int (повертає площу) та str (повертає інформацію про фігуру).

--- main.py
+++ main.py
@@ -1,16 +1,75 @@
-# This is a sample Python script.
+import math
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+class Shape:
+    def calculate_area(self):
+        pass
+
+    def __int__(self):
+        return int(round(self.calculate_area()))
+
+    def __str__(self):
+        return "Shape"
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def calculate_area(self):
+        return self.length * self.width
+
+    def __str__(self):
+        return f"Rectangle - Length: {self.length}, Width: {self.width}"
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def calculate_area(self):
+        return math.pi * self.radius ** 2
+
+    def __str__(self):
+        return f"Circle - Radius: {self.radius}"
+
+class RightTriangle(Shape):
+    def __init__(self, base, height):
+        self.base = base
+        self.height = height
+
+    def calculate_area(self):
+        return 0.5 * self.base * self.height
+
+    def __str__(self):
+        return f"Right Triangle - Base: {self.base}, Height: {self.height}"
+
+class Trapezoid(Shape):
+    def __init__(self, upper_base, lower_base, height):
+        self.upper_base = upper_base
+        self.lower_base = lower_base
+        self.height = height
+
+    def calculate_area(self):
+        return 0.5 * (self.upper_base + self.lower_base) * self.height
+
+    def __str__(self):
+        return f"Trapezoid - Upper Base: {self.upper_base}, Lower Base: {self.lower_base}, Height: {self.height}"
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Example of using the classes:
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+rectangle = Rectangle(5, 8)
+print(f"Area of the rectangle: {int(rectangle)}")
+print(str(rectangle))
+
+circle = Circle(3)
+print(f"Area of the circle: {int(circle)}")
+print(str(circle))
+
+right_triangle = RightTriangle(4, 6)
+print(f"Area of the right triangle: {int(right_triangle)}")
+print(str(right_triangle))
+
+trapezoid = Trapezoid(5, 7, 10)
+print(f"Area of the trapezoid: {int(trapezoid)}")
+print(str(trapezoid))
