805661b701ecbed85202439480d8c0e8e3b9ad29
yeyevtushenko <yehor803@gmail.com>
2023-12-25 20:47:49 +0200

    Створіть базовий клас «Фігура» з методом для підрахунку площі. Створіть похідні класи: прямокутник, коло, прямокутний трикутник, трапеція, зі своїми методами для підрахунку площі.

--- main.py
+++ main.py
@@ -1,16 +1,56 @@
-# This is a sample Python script.
+import math
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+class Shape:
+    def calculate_area(self):
+        pass
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    def calculate_area(self):
+        return self.length * self.width
+
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def calculate_area(self):
+        return math.pi * self.radius ** 2
+
+
+class RightTriangle(Shape):
+    def __init__(self, base, height):
+        self.base = base
+        self.height = height
+
+    def calculate_area(self):
+        return 0.5 * self.base * self.height
+
+
+class Trapezoid(Shape):
+    def __init__(self, upper_base, lower_base, height):
+        self.upper_base = upper_base
+        self.lower_base = lower_base
+        self.height = height
+
+    def calculate_area(self):
+        return 0.5 * (self.upper_base + self.lower_base) * self.height
+
+
+
+
+rectangle = Rectangle(5, 8)
+print(f"Area of the rectangle: {rectangle.calculate_area()}")
+
+circle = Circle(3)
+print(f"Area of the circle: {circle.calculate_area()}")
+
+right_triangle = RightTriangle(4, 6)
+print(f"Area of the right triangle: {right_triangle.calculate_area()}")
+
+trapezoid = Trapezoid(5, 7, 10)
+print(f"Area of the trapezoid: {trapezoid.calculate_area()}")
